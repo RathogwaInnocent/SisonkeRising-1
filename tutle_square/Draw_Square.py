dca5fcec69daf2acef920b0c81a2def30c9ee37a
Florah <f.n.weni@gmail.com>
2016-05-14 04:44:01 -0400

    our turtle draw_square function

---
+++ tutle_square/Draw_Square.py
@@ -0,0 +1,15 @@
+def draw_square(size,colour):
+	color(colour)
+	go(size)
+	turn(90)
+	go(size)
+	turn(90)
+	go(size)
+	turn(90)
+	go(size)
+	
+draw_square(50,"blue")
+
+reset()
+
+draw_square(150,"green")
